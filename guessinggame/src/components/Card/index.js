82c3520dedc412e7e09658eee8d1435918e842dd
Gabe Sucich <gabe.sucich@gmail.com>
2020-08-08 22:21:48 -0700

    Adding card React component

---
+++ guessinggame/src/components/Card/index.js
@@ -0,0 +1,13 @@
+import "./style.css"
+
+function Card(props) {
+    return (
+        <div className="col s6 m4 l3">
+            <div className="card blue-grey darken-1">
+                <div class="card-image">
+                    <img src={this.props.src}/>
+                </div>
+            </div>
+        </div>
+    )
+}
