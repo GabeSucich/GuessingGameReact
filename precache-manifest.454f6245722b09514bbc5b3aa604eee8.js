d0f7ba11afe376d5270615002e92921839e8dcf0
Gabe Sucich <gabe.sucich@gmail.com>
2020-08-11 10:48:52 -0700

    Updates

---
+++ precache-manifest.454f6245722b09514bbc5b3aa604eee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe316da7da26ec810b1fd75fbef5265a",
+    "url": "/GuessingGameReact/index.html"
+  },
+  {
+    "revision": "b74b6d3edbfd52de1e4f",
+    "url": "/GuessingGameReact/static/css/main.d92c1089.chunk.css"
+  },
+  {
+    "revision": "25ac120f703cfeb52a3a",
+    "url": "/GuessingGameReact/static/js/2.de61a5b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GuessingGameReact/static/js/2.de61a5b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b74b6d3edbfd52de1e4f",
+    "url": "/GuessingGameReact/static/js/main.de68264b.chunk.js"
+  },
+  {
+    "revision": "a355a238cf29b1ca98c1",
+    "url": "/GuessingGameReact/static/js/runtime-main.78981cc8.js"
+  }
+]);
